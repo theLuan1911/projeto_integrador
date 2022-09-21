a1cf22c779581367ada69fd9c514cf33cb08bb00
theLuan1911 <luan.bsilva098@gmail.com>
2022-09-21 16:52:55 -0300

    2.0

--- projeto_integrador/core/views/carrinho.php
+++
@@ -1,1 +0,0 @@
-<h3>Carrinho!</h3>
--- projeto_integrador/core/views/inicio.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-
-        <h1>Página Inicial da Loja</h1>
-
-        </div>
-    </div>
-</div>
